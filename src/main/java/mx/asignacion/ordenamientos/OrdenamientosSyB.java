02a9065832807982c2828bf69aea07b16ebcd903
manuel hernandez <manuel.hernandez267661@potros.itson.edu.mx>
2026-03-17 13:54:09 -0700

    Implementacion de la logica del algoritmo de insercion

--- src/main/java/mx/asignacion/ordenamientos/OrdenamientosSyB.java
+++ src/main/java/mx/asignacion/ordenamientos/OrdenamientosSyB.java
@@ -70,11 +70,22 @@ public class OrdenamientosSyB {
          * T(n) = a*n^2 + b*n + c
          * Notación Asintótica: O(n^2)
          */
-    } // Aquí termina el método burbuja
-
-} // <--- ¡AQUÍ TERMINA LA CLASE!
-
+    }
 // Método de Inserción (Estructura base)
     public void insercion(int[] arr) {
-        System.out.println("El algoritmo de inserción se implementará en el siguiente commit...");
+        int n = arr.length; 
+        
+        for (int i = 1; i < n; i++) { 
+            int key = arr[i]; 
+            int j = i - 1;    
+            
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1;           
+            }
+            arr[j + 1] = key; 
+        }
+    
     }
+}
+
