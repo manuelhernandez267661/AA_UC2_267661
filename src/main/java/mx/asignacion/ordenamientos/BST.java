f52665d177f735c6c115d178e38f427a12b2a4ab
manuel hernandez <manuel.hernandez267661@potros.itson.edu.mx>
2026-04-30 14:59:15 -0700

    se implementó insercion de elementos

--- src/main/java/mx/asignacion/ordenamientos/BST.java
+++ src/main/java/mx/asignacion/ordenamientos/BST.java
@@ -9,5 +9,40 @@ package mx.asignacion.ordenamientos;
  * @author isaac
  */
 class BST {
-
+    class Nodo {
+        int valor;
+        Nodo izquierdo, derecho;
+        
+        public Nodo(int item){
+            valor = item;
+            izquierdo = derecho = null;
+        }
+    }
+    Nodo raiz;
+    // Constructor del árbol vacío
+    public BST() {
+        raiz = null;
+    }
+    
+    // a. Inserción de elementos
+    // Propósito: Método público para insertar un nuevo valor en el árbol.
+    public void insertar(int valor) {
+        raiz = insertarRecursivo(raiz, valor);
+    }
+    private Nodo insertarRecursivo(Nodo raiz, int valor) {
+        // Si el árbol/subárbol está vacío, retorna un nuevo nodo
+        if (raiz == null) {
+            raiz = new Nodo(valor);
+            return raiz;
+        }
+        // De lo contrario, recorre el árbol
+        if (valor < raiz.valor) {
+            raiz.izquierdo = insertarRecursivo(raiz.izquierdo, valor);
+        } else if (valor > raiz.valor) {
+            raiz.derecho = insertarRecursivo(raiz.derecho, valor);
+        }
+    // Retorna el puntero al nodo (sin cambios)
+        return raiz;
+    }
 }
+
